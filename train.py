afa847ae35ad8a8c1edabd70812860ee39c614e9
Juan Marcos Ottonello <jmottonello@gmail.com>
2017-02-05 22:58:48 +0100

    Moving model to train.py

--- train.py
+++ train.py
@@ -1,5 +1,9 @@
 from keras.optimizers import Adam
-from models import basic, nv, test
+from keras.models import Sequential
+from keras.layers.core import Dense, Activation, Flatten, Dropout
+from keras.layers import Convolution2D, ZeroPadding2D, MaxPooling2D
+from keras.layers.core import Lambda
+from keras.layers.advanced_activations import ELU
 from generators import generate_next_batch
 from keras import backend as K
 import pandas as pd
@@ -19,6 +23,42 @@ number_of_epochs = 11
 batch_size = 64
 number_of_samples_per_epoch = 20032
 validation_split = 0.3
+
+
+# Input layer shape
+ch, row, col = 3, 64, 64
+
+def basic():
+    model = Sequential()
+    model.add(Lambda(lambda x: x/127.5 - 1.,
+            input_shape=(col,row,ch),
+            output_shape=(col,row,ch)))
+    model.add(Convolution2D(16, 8, 8, subsample=(4, 4), border_mode="same"))
+    model.add(Activation('relu'))
+    model.add(Convolution2D(32, 3, 3, subsample=(2, 2), border_mode="same"))
+    model.add(MaxPooling2D(pool_size=(2, 2), strides=None, border_mode='valid', dim_ordering='default'))
+    model.add(Activation('relu'))
+    model.add(Convolution2D(32, 5, 5, border_mode="same"))
+    model.add(Activation('relu'))
+    model.add(Convolution2D(64, 3, 3, border_mode="same"))
+    model.add(MaxPooling2D(pool_size=(2, 2), strides=None, border_mode='valid', dim_ordering='default'))
+    model.add(Activation('relu'))
+    model.add(Convolution2D(64, 5, 5, subsample=(2, 2), border_mode="same"))
+    model.add(Flatten())
+    model.add(Dropout(.2))
+    model.add(Activation('relu'))
+    model.add(Dense(400))
+    model.add(Dropout(.5))
+    model.add(Activation('relu'))
+    model.add(Dense(100))
+    model.add(Dropout(.5))
+    model.add(Activation('relu'))
+    model.add(Dense(20))
+    model.add(Dropout(.5))
+    model.add(Activation('relu'))
+    model.add(Dense(1))
+
+    return model
 
 # construct the selected model and print it to the screen
 model = basic()
@@ -55,3 +95,97 @@ with open(OUTPUT_MODEL_FILE, "w") as json_file:
 model.save(OUTPUT_WEIGHTS_FILE)
 
 K.clear_session()
+
+
+
+
+###
+## Other Model definitions
+###
+
+def nv():
+    model = Sequential()
+    model.add(Lambda(lambda x: x/127.5 - 1.,
+            input_shape=(col,row,ch),
+            output_shape=(col,row,ch)))
+    model.add(Convolution2D(24, 5, 5, subsample=(2, 2), border_mode="same"))
+    model.add(Activation('relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2), strides=(1, 1)))
+    
+    model.add(Convolution2D(36, 5, 5, subsample=(2, 2), border_mode="same"))
+    model.add(Activation('relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2), strides=(1, 1)))
+
+    model.add(Convolution2D(48, 5, 5, subsample=(2, 2), border_mode="same"))
+    model.add(Activation('relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2), strides=(1, 1)))
+
+    model.add(Convolution2D(64, 3, 3, border_mode="same"))
+    model.add(Activation('relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2), strides=(1, 1)))
+
+    model.add(Convolution2D(64, 3, 3, border_mode="same"))
+    model.add(Activation('relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2), strides=(1, 1)))
+    
+    model.add(Flatten())
+    model.add(Dropout(.2))
+    model.add(Activation('relu'))
+    model.add(Dense(1164))
+    model.add(Dropout(.5))
+    model.add(Activation('relu'))
+    model.add(Dense(100))
+    model.add(Dropout(.5))
+    model.add(Activation('relu'))
+    model.add(Dense(50))
+    model.add(Dropout(.5))
+    model.add(Activation('relu'))
+    model.add(Dense(10))
+    model.add(Dropout(.5))
+    model.add(Activation('relu'))
+    model.add(Dense(1))
+    return model
+
+def test():
+    
+    model = Sequential()
+    model.add(Lambda(lambda x: x/127.5 - 1.,
+            input_shape=(col,row,ch),
+            output_shape=(col,row,ch)))
+    model.add(Convolution2D(24, 5, 5, subsample=(2, 2), border_mode="same"))
+    model.add(ELU())
+    model.add(MaxPooling2D(pool_size=(2, 2), strides=(1, 1)))
+    
+    model.add(Convolution2D(36, 5, 5, subsample=(2, 2), border_mode="same"))
+    model.add(ELU())
+    model.add(MaxPooling2D(pool_size=(2, 2), strides=(1, 1)))
+
+    model.add(Convolution2D(48, 5, 5, subsample=(2, 2), border_mode="same"))
+    model.add(ELU())
+    model.add(MaxPooling2D(pool_size=(2, 2), strides=(1, 1)))
+
+    model.add(Convolution2D(64, 3, 3, border_mode="same"))
+    model.add(ELU())
+    model.add(MaxPooling2D(pool_size=(2, 2), strides=(1, 1)))
+
+    model.add(Convolution2D(64, 3, 3, border_mode="same"))
+    model.add(ELU())
+    model.add(MaxPooling2D(pool_size=(2, 2), strides=(1, 1)))
+    
+    model.add(Flatten())
+    model.add(Dropout(.2))
+    model.add(ELU())
+    model.add(Dense(1164))
+    model.add(Dropout(.5))
+    model.add(ELU())
+    model.add(Dense(100))
+    model.add(Dropout(.5))
+    model.add(ELU())
+    model.add(Dense(50))
+    model.add(Dropout(.5))
+    model.add(ELU())
+    model.add(Dense(10))
+    model.add(Dropout(.5))
+    model.add(ELU())
+    model.add(Dense(1))
+    return model
